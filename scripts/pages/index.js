f524d93f0a43a023089db093cd00c8a54b850dda
Jun <junhan.dev@gmail.com>
2023-04-27 23:12:11 +0200

    display items et styles
    but je dois use fetch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,70 @@
-async function getPhotographers() {
-  // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
-  // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-  let photographers = [
-    {
-      name: "Ma data test",
-      id: 1,
-      city: "Paris",
-      country: "France",
-      tagline: "Ceci est ma data test",
-      price: 400,
-      portrait: "account.png",
-    },
-    {
-      name: "Autre data test",
-      id: 2,
-      city: "Londres",
-      country: "UK",
-      tagline: "Ceci est ma data test 2",
-      price: 500,
-      portrait: "account.png",
-    },
-  ];
-  // et bien retourner le tableau photographers seulement une fois récupéré
-  return {
-    photographers: [...photographers, ...photographers, ...photographers],
-  };
-}
+import data from "../../data/photographers.json" assert { type: "json" };
 
-async function displayData(photographers) {
-  const photographersSection = document.querySelector(".photographer_section");
+const container = document.querySelector(".photographer_section");
 
-  photographers.forEach((photographer) => {
-    const photographerModel = photographerFactory(photographer);
-    const userCardDOM = photographerModel.getUserCardDOM();
-    photographersSection.appendChild(userCardDOM);
-  });
-}
+const display = () => {
+  const newFollowers = data.photographers
+    .map((person) => {
+      const { name, city, country, portrait, price, tagline, id } = person;
+      return `
+        <article>
+          <img src="../../assets/images/Photographers ID Photos/${portrait}" alt="${id}"/>
+          <h2>${name}</h2>
+          <h4>${city}, ${country}</h4>
+          <span>${tagline}</span>
+          <span>${price}€/jour</span>
+        </article>
+        `;
+    })
+    .join("");
+  container.innerHTML = newFollowers;
+};
 
-async function init() {
-  // Récupère les datas des photographes
-  const { photographers } = await getPhotographers();
-  displayData(photographers);
-}
+display();
 
-init();
+// async function getPhotographers() {
+//   // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet,
+//   // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
+//   let photographers = [
+//     {
+//       name: "Ma data test",
+//       id: 1,
+//       city: "Paris",
+//       country: "France",
+//       tagline: "Ceci est ma data test",
+//       price: 400,
+//       portrait: "account.png",
+//     },
+//     {
+//       name: "Autre data test",
+//       id: 2,
+//       city: "Londres",
+//       country: "UK",
+//       tagline: "Ceci est ma data test 2",
+//       price: 500,
+//       portrait: "account.png",
+//     },
+//   ];
+//   // et bien retourner le tableau photographers seulement une fois récupéré
+//   return {
+//     photographers: [...photographers, ...photographers, ...photographers],
+//   };
+// }
+
+// async function displayData(photographers) {
+//   const photographersSection = document.querySelector(".photographer_section");
+
+//   photographers.forEach((photographer) => {
+//     const photographerModel = photographerFactory(photographer);
+//     const userCardDOM = photographerModel.getUserCardDOM();
+//     photographersSection.appendChild(userCardDOM);
+//   });
+// }
+
+// async function init() {
+//   // Récupère les datas des photographes
+//   const { photographers } = await getPhotographers();
+//   displayData(photographers);
+// }
+
+// init();
